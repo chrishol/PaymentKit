a732ea4c929fbf67141d0901c48966a9cdf1bc23
Chris Hollindale <chris.hollindale@gmail.com>
2013-10-06 22:12:58 -0700

    Add a hitTest override to make the control easier to work with

--- PaymentKit/PKTextField.m
+++ PaymentKit/PKTextField.m
@@ -16,4 +16,17 @@
     [[self placeholder] drawInRect:rect withFont:self.placeholderFont];
 }
 
+// Override hitTest so that we have a larger area to tap on, but don't interfere with Apple's standard overlay stuff - call super first
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    UIView *hitView = [super hitTest:point withEvent:event];
+    if (hitView) {
+        return hitView;
+    } else {
+        int errorMargin = 8;
+        CGRect largerFrame = CGRectMake(0 - errorMargin, 0 - errorMargin, self.frame.size.width + 2*errorMargin, self.frame.size.height + 2*errorMargin);
+        return (CGRectContainsPoint(largerFrame, point) == 1) ? self : nil;
+    }
+}
+
 @end
